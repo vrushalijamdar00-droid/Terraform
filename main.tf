019d92e22602eb48a4f1ff7a70ea688ba67e1691
Your Name <your-email@example.com>
2026-09-05 12:10:02 +0530

    forth commit

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-resource "aws_ami" "demoami"{
-    Name = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20260610"
-    source_instance_id = "i-0fc4816e4519f9b7f"
+resource "aws_ami_from_instance" "demoami" {
+  name               = "demo-ubuntu-ami"
+  source_instance_id = "i-0fc4816e4519f9b7f"
+
+  tags = {
+    Name = "demo-ubuntu-ami"
+  }
 }

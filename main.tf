6db7d64ee2c50d2079f51863ca0112c28b7335c5
Your Name <your-email@example.com>
2026-09-05 11:58:58 +0530

    Third commit

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "aws_ami" "demoami"{
-    Name = "demo-ami"
+    Name = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20260610"
     source_instance_id = "i-0fc4816e4519f9b7f"
 }
